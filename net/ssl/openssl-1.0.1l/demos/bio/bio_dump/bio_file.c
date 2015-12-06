9d15b6e7ead487e2859f2cf31ad8c7e7b4e85db3
hehongjie <hehj@koal.com>
2015-12-06 22:36:40 +0800

    修改：openssl

---
+++ net/ssl/openssl-1.0.1l/demos/bio/bio_dump/bio_file.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/bio.h>
+#include <openssl/asn1.h>
+
+int main(void)
+{
+    int ret;
+    BIO *bp;
+    char buf[] = "test";
+
+    bp=BIO_new_file ("./sample.log", "wb");
+
+    BIO_printf(bp, "%s", buf);
+
+    BIO_free(bp);
+    return 0;
+}
+
+
